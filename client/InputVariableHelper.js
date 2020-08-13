9f21b28b32f8fc47f523d50e916f2a8fcb23c731
Maximilian Trumpf <trumpf.max@gmail.com>
2020-08-13 11:23:16 +0200

    feat(variableExtraction): return downstream decisions

--- client/InputVariableHelper.js
+++ client/InputVariableHelper.js
@@ -9,27 +9,37 @@ import { is } from 'dmn-js-shared/lib/util/ModelUtil';
 export function getInputVariables(container) {
 
   // (1) get decisions
-  const decisions = container.drgElement.filter((element) => element && element.$type === 'dmn:Decision');
+  const decisions = container.drgElement.filter((element) => 
+    element && 
+    element.$type === 'dmn:Decision' 
+    && is(element.decisionLogic, 'dmn:DecisionTable'));
 
-  // (2) for each decision, map name, variables and their types
+  // (2) get decisions dependencies as map with depth = 1
+  let downstreamDecisions = { };
+  // TODO implement as more performant for loop
+  decisions.forEach(decision => {
+    downstreamDecisions[decision.id] = (
+      decision.informationRequirement && 
+      decision.informationRequirement.length > 0 &&
+      decision.informationRequirement.filter(infoReq => infoReq.requiredDecision).length > 0) ? 
+      decision.informationRequirement.filter(infoReq => infoReq.requiredDecision)
+        .map(infoReq => infoReq.requiredDecision.href.slice(1)) : 
+    [];
+  });
+
+  // (3) for each decision, map name, variables and their types. Also add the downstream decisions
   const result = decisions.map((decision) => {
     const { decisionLogic } = decision;
-
-    if (!is(decisionLogic, 'dmn:DecisionTable')) {
-      return {
-        decision: decision.name,
-        variables: []
-      };
-    }
-
     return {
       'decision': decision.name,
+      'decisionId': decision.id,
       'variables': decision.decisionLogic.input.map((input) => {
         return {
           'name': input.label,
           'type': input.inputExpression.typeRef
-        };
-      })
+        }
+      }),
+      'downstreamDecisions': downstreamDecisions[decision.id]
     };
   });
 
--- test/InputVariableHelperSpec.js
+++ test/InputVariableHelperSpec.js
@@ -20,6 +20,7 @@ describe('getInputVariables', function() {
     expect(decisions.length).to.be.equal(2);
   });
 
+
   it('should parse variable types and decision structure', async function() {
 
     // given
@@ -33,8 +34,38 @@ describe('getInputVariables', function() {
     expect(decisions[0].variables[0]).to.include({ 'name': 'inputVar1', 'type': 'string' });
     expect(decisions[0].variables[1]).to.include({ 'name': 'inputVar2', 'type': 'boolean' });
     expect(decisions[0].variables[2]).to.include({ 'name': 'inputVar3', 'type': 'integer' });
-    console.log(decisions);
   });
+
+
+  it('should parse downstream decisions with depth = 1', async function() {
+
+    // given
+    const definitions = await read('./test/DependentDecisionsDiagram.dmn');
+
+    // when
+    const decisions = getInputVariables(definitions);
+
+    console.log(decisions);
+
+    // then
+    expect(decisions.length).to.be.equal(3);
+    expect(decisions[1].downstreamDecisions.length).to.be.equal(2);
+    expect(decisions[1].downstreamDecisions[0]).to.be.equal('decision_1');
+  });
+
+
+  it('should ignore input expressions', async function() {
+
+    // given
+    const definitions = await read('./test/LiteralExpressionTest.dmn');
+
+    // when
+    const decisions = getInputVariables(definitions);
+
+    // then
+    expect(decisions.length).to.be.equal(1);
+  });
+
 });
 
 // helpers
